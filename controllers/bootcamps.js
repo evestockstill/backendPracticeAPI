8e980f7a53432792a7cb32faaf3ec586097e4f09
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-27 21:44:13 -0800

    laying out files middleware mongoDB

---
+++ controllers/bootcamps.js
@@ -0,0 +1,30 @@
+// @desc Get all bootcamps
+// @route GET /api/v1/bootcamps
+// @access Public
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'show all bootcamps' });
+};
+// @desc Get single bootcamp
+// @route GET /api/v1/bootcamps/"id"
+// @access Public
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `get one bootcamps ${req.params.id}` });
+};
+// @desc create bootcamps
+// @route POST /api/v1/bootcamps/"id"
+// @access private
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'create bootcamp' });
+};
+// @desc update bootcamps
+// @route PUT /api/v1/bootcamps/"id"
+// @access private
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `update bootcamps ${req.params.id}` });
+};
+// @desc delete bootcamp
+// @route DELETE /api/v1/bootcamps/"id"
+// @access Public
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `delete ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 /* eslint-disable no-undef */
+
+// require('dotenv').config();
 const express = require('express');
-const dontenv = require('dotenv');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+// route files
+dotenv.config({ path: './config/.env' });
+connectDB();
 
-dontenv.config({ path: './config/config.env' });
 
+
+const bootcamps = require('./routes/bootcamps');
 const app = express();
+// dev logging middleware
+
+app.use(morgan('dev'));
+
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, 
-    // eslint-disable-next-line no-undef
-    console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
+app.listen(PORT, () => {
+    console.log(`server running on ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold);
+});
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+});
